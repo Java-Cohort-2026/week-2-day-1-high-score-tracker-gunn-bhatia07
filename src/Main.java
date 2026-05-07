6052f49f4168b489780bf051f1351d5a94d21b93
Gunn Bhatia <gunnbhatia07@gmail.com>
2026-05-07 08:49:26 +0530

    W1 D1 Done!

--- src/Main.java
+++ src/Main.java
@@ -14,10 +14,16 @@ public class Main {
         // 3. Inside the loop, check if the current score is greater than 'highestScore'
         
         // TODO: Write your loop here!
-
+        for(int i=0; i<scores.length; i++) {
+            totalSum += scores[i];
+            if (scores[i] > highestScore) {
+                highestScore = scores[i];
+            }
+        }
 
         // 4. After the loop, calculate the average (Hint: be careful with integer division!)
         // TODO: Calculate averageScore here!
+        averageScore = (double) totalSum / scores.length;
 
         // --- OUTPUT ---
         System.out.println("The highest score is: " + highestScore);
